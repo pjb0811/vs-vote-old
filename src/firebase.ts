4f9a22d1f2ce317a67bd75e71972d818c66abe32
jbpark <jbpark@wemakeprice.com>
2018-01-29 14:38:32 +0900

    setting firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCeq1vCAwhiO9Bpj7op3KPpQqRyc1pcaZM',
+  authDomain: 'versus-vote.firebaseapp.com',
+  databaseURL: 'https://versus-vote.firebaseio.com',
+  projectId: 'versus-vote',
+  storageBucket: 'versus-vote.appspot.com',
+  messagingSenderId: '283112969857'
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
